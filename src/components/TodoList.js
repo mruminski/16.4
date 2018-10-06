8d259fad49d88dc9100046f079b93e10810b873a
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-10-06 18:32:11 +0200

    Add semicolon

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,7 @@ import style from './TodoList.css'
 const TodoList = ({list, method }) => 
   (<ul className={style.TodoList}>
     {(list.length > 0 && list.map(i =>
-      <li key={i.id}>{i.text}</li>))}
+      <li key={i.id}>{i.text}</li>))};
     </ul>)
 
 export default TodoList;
