bdf5ef31f46bfdac05bbb3140c196fc36dae0182
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-10-06 14:15:15 +0200

    Refactor TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const TodoList = ({list, method }) => 
   (<ul className='todo__list'>
-  {(list.length > 0 && list.map(i => <li className='todo__item' key={i.id}>{i.text}</li>))}
+  {(list.length > 0 && list.map(i =>
+    <li className='todo__item' key={i.id}>{i.text}</li>))};
   </ul>)
 
 export default TodoList;
