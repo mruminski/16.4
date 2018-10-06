446e2874632a76c865aeb75dfbe2ca3f63cceff1
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-10-06 18:51:53 +0200

    Add and configure HMR

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,10 @@ module.exports = env => {
       rules: [
         {
           test: /\.js$/,
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            plugins: env !== 'production' ? ['react-hot-loader/babel'] : []
+          }
         },
         {
           test: /\.css$/,
