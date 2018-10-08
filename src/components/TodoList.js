82e060f2228d639b9dae57202525f5c3dddf45be
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-10-08 06:05:00 +0200

    Remove empty line and fix ; problem

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import style from './TodoList.css'
 
-
 const TodoList = ({list, method }) => 
   (<ul className={style.TodoList}>
     {(list.length > 0 && list.map(i =>
-      <li key={i.id}>{i.text}</li>))};
-    </ul>)
+      <li key={i.id}>{i.text}</li>))}
+    </ul>);
 
 export default TodoList;
